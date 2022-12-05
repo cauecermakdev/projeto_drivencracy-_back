b1ed63efb609fd16c470fa0d6149de4664983ebd
Cauê Pereira Cermak <cauecermak@gmail.com>
2022-12-05 14:53:13 -0300

    render settings

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(5000,()=>{
+/* app.listen(5000,()=>{
     console.log("server is listening on port 5000");
 })
+ */
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
